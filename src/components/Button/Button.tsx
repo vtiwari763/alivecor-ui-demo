bd6a29c3cc22f3add0df8804cd58a75224359e73
vtiwari763 <vtiwari763@alivecor.com>
2024-07-08 14:05:33 +0530

    Vivek | Initial button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,48 +1,24 @@
-import React from 'react';
-import './button.css';
+import * as React from "react";
+import MUIButton from "@mui/material/Button";
+import "./button.css";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  size?: "small" | "medium" | "large";
+  variant?: "text" | "contained" | "outlined";
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  disabled?: boolean;
+  onClick?: any;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
+export function Button({
   label,
+  variant = "contained",
+  size = "medium",
   ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+}: ButtonProps) {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <MUIButton variant={variant} size={size} {...props}>
       {label}
-    </button>
+    </MUIButton>
   );
-};
+}
