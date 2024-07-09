1ed3dbf26cc5897bb536513497fdb8c7ef339f99
vtiwari763 <vtiwari763@alivecor.com>
2024-07-09 10:11:50 +0530

    MUI theme configure

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,11 @@
 import type { Preview } from "@storybook/react";
 
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+
+/* TODO: update import for your custom Material UI themes */
+import { theme } from "../src/theme";
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +15,19 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        light: theme,
+        dark: theme,
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
